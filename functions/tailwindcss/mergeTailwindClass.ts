08bce0031e40b7fe3076a7b405434be2c9d9083d
edsolater <edsolater@qq.com>
2021-08-28 19:25:37 +0800

    create mergeTailwindClass

--- functions/tailwindcss/mergeTailwindClass.ts
+++ functions/tailwindcss/mergeTailwindClass.ts
@@ -1,26 +1,66 @@
-import tailwindClassMap from './tailwindClassMap'
+import tailwindClassMap from './tailwindFeaturePart.json'
 
-const getCSSPropertyName = (tailwindFeatureClass: string) => tailwindClassMap.get(tailwindFeatureClass)
+const getCSSPropertyName = (tailwindFeatureClass: string): string =>
+  tailwindClassMap[tailwindFeatureClass]
 
-const getFeatureName = (tailwindClass: string) =>
-  tailwindClass
-    .replace(/\[.*\]/, '')
-    .replace(/-\d+/, '')
-    .replace(/^-/, '')
-    .replace(/-$/, '')
-// TODO: haven't consider about  css shortcut \ taildwind class to multi css properties yet
 /**
  *
  * @example
  * mergeTailwindClass('m-4', '-mx-5', 'flex flex-row', 'justify-between justify-around justify-items-center') //=> 'm-4 -mx-5 flex flex-row justify-around justify-items-center'
+ * mergeTailwindClass('flex sm:-mx-5 justify-between justify-around', 'grid sm:mx-2 dark:lg:mx-5') //=> 'grid sm:mx-2 justify-around dark:lg:mx-5' 
  *
  */
-export function mergeTailwindClass(...longClassNames: string[]) {
+export default function mergeTailwindClass(...longClassNames: string[]): string {
   const allClassNames = longClassNames
     .flatMap((l) => l.split(' '))
-    .map((c, idx) => ({ originalClass: c, cssProperty: getCSSPropertyName(getFeatureName(c)), index: idx }))
+    .map((c) => ({
+      originalClass: c,
+      cssProperty: getCSSPropertyName(getTailwindClassFeaturePart(c)),
+      prefix: getClassNameVariant(c)
+    }))
   const uniqueClassNameMap = new Map(
-    allClassNames.map(({ cssProperty, originalClass }) => [cssProperty, originalClass])
+    allClassNames.map(({ cssProperty, originalClass, prefix }) => [
+      [...prefix, cssProperty].join(''),
+      originalClass
+    ])
   )
   return [...uniqueClassNameMap.values()].join(' ')
+}
+
+
+function getClassNameVariant(selector: string) {
+  return Array.from(selector.match(/([\w-]+):/g) || [])
+}
+
+function getTailwindClassFeaturePart(selector: string) {
+  const main = selector
+    .replace(/^-/, '')
+    .replace(/[\w-]+\\:/g, '')
+    .replace(/::?[\w-]+/g, '')
+  const featureString = main
+    .split('-')
+    .map((t) => (isTailwindClassText(t) ? t : ''))
+    .join('-')
+  return featureString
+}
+
+function isTailwindClassText(text: string) {
+  const sizeKeywords = [
+    'xs',
+    'sm',
+    'md',
+    'lg',
+    'xl',
+    '2xl',
+    '3xl',
+    '4xl',
+    '5xl',
+    '6xl',
+    '7xl',
+    '8xl',
+    '9xl'
+  ]
+  const lengthKeywords = ['full', 'px', 'screen']
+  const keywords = [...sizeKeywords, ...lengthKeywords]
+  return text && !keywords.includes(text) && !/\[.+\]/.test(text) && !/^[\d|\\|\.|/]+$/.test(text)
 }
--- functions/tailwindcss/parseCSS.mjs
+++
@@ -1,2 +0,0 @@
-import longText from './utilities.txt'
-console.log(String(longText).slice(0, 8))
