549a59d158ecda7fc082e048014b8776ccc94aca
edsolater <edsolater@qq.com>
2021-09-07 00:55:15 +0800

    add useBFlag()

--- hooks/useToggle.tsx
+++ hooks/useToggle.tsx
@@ -1,19 +1,23 @@
-import { useCallback, useMemo, useRef, useState } from 'react'
+import { useCallback, useMemo, useRef } from 'react'
+import useBFlag from './useBFlag'
+
 type MayFunc<T, Params extends any[] = any[]> = T | ((...params: Params) => T)
 /**
  * it too widely use that there should be a hook
  * @param initValue
+ *
+ * @deprecated please use `useBFlay()` instead. for `useToggle()` should make a `state` and a `controller`. whitch is tedious to use.
  */
 export default function useToggle(
   initValue: MayFunc<boolean> = false,
-  options: {
+  options?: {
     /* usually it is for debug */
     onOff?(): void
     /* usually it is for debug */
     onOn?(): void
     /* usually it is for debug */
     onToggle?(): void
-  } = {}
+  }
 ): [
   boolean,
   {
@@ -23,42 +27,33 @@ export default function useToggle(
     set: (b: boolean) => void
   }
 ] {
-  const [isOn, _setIsOn] = useState(initValue)
-  const setIsOn = (...params: any[]) => {
-    //@ts-expect-error temp
-    _setIsOn(...params)
-  }
-  const on = useCallback(() => {
-    setIsOn(true)
-    options.onOn?.()
-  }, [])
-  const off = useCallback(() => {
-    setIsOn(false)
-    options.onOff?.()
-  }, [])
-  const toggle = useCallback(() => {
-    setIsOn((b: any) => !b)
-    options.onToggle?.()
-  }, [])
+  const bFlag = useBFlag(initValue, {
+    onChange: (v) => {
+      if (v) {
+        options?.onOn?.()
+      } else {
+        options?.onOff?.()
+      }
+      options?.onToggle?.()
+    }
+  })
   const controller = useMemo(
     () => ({
-      on,
-      off,
-      toggle,
-      set: setIsOn
+      on: bFlag.on,
+      off: bFlag.off,
+      toggle: bFlag.toggle,
+      set: bFlag.set
     }),
-    [off, on, toggle, setIsOn]
+    [bFlag]
   )
-  return [isOn, controller]
+  return [bFlag.value(), controller]
 }
 
 /**
  * useRef but use similar API
  * @param initValue
  */
-export function useToggleRef(
-  initValue: MayFunc<boolean> = false
-): [
+export function useToggleRef(initValue: MayFunc<boolean> = false): [
   () => boolean,
   {
     on: () => void
