f9b0fade34213a42b29d6323301d796f78e42db6
edsolater <edsolater@qq.com>
2021-09-07 01:09:01 +0800

    tedius to write , but easier to read

--- uikit/Button.tsx
+++ uikit/Button.tsx
@@ -1,22 +1,44 @@
-import type { UseButtonClassName } from '../hooks/classNameHooks/useButtonClassName'
 import type { DivProps } from './Div'
 import UIRoot from './UIRoot'
-import useButtonClassName from '../hooks/classNameHooks/useButtonClassName'
 import { useClickableElementRef } from '../hooks/useClickableElement'
 
-export interface ButtonProps extends DivProps<'button'>, UseButtonClassName {
+export interface ButtonProps extends DivProps<'button'> {
   onClick?: () => void
 }
 
+export const buttonBaseStyle = ({
+  size = 'medium',
+  type = 'fill'
+}: {
+  /**
+   * @default 'fill'
+   */
+  type?: 'fill' | 'outline' | 'text'
+  /**
+   * @default 'medium'
+   */
+  size?: 'small' | 'medium' | 'large'
+} = {}) =>
+  [
+    'appearance-none cursor-pointer select-none w-max',
+    {
+      small: 'py-0 px-2 text-sm rounded',
+      medium: 'py-1.5 px-4 text-base rounded-md',
+      large: 'py-3 px-6 text-lg rounded-lg'
+    }[size],
+    {
+      outline: `relative ring-inset ${
+        size === 'large' ? 'ring-2' : size === 'small' ? 'ring-1' : 'ring-2'
+      } ring-opacity-80 ring-fill-dark`,
+      fill: 'relative bg-fill-dark text-text-light hover:brightness-125 active:brightness-90',
+      text: 'text-text-dark bg-transparent'
+    }[type]
+  ].join(' ')
+
 export default function Button({ onClick, ...restProps }: ButtonProps) {
-  const defaultClassName = useButtonClassName(restProps)
   const ref = useClickableElementRef({ onClick })
   return (
-    <UIRoot
-      {...restProps}
-      _className={[Button.name, defaultClassName, restProps.className]}
-      _domRef={ref}
-    >
+    <UIRoot {...restProps} _className={Button.name} _domRef={ref}>
       {restProps.children ?? '🤨'}
     </UIRoot>
   )
