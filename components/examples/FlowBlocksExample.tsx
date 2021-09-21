b546e4d0e615521ddf70b7a79631934f83e0d63e
zhanggongying <edsolater@qq.com>
2021-09-22 00:03:28 +0800

    还未处理衰减

--- components/examples/FlowBlocksExample.tsx
+++ components/examples/FlowBlocksExample.tsx
@@ -1,48 +1,31 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect } from 'react'
 import { genGridTemplate } from '../../styles/styleSnippets'
 import Card from '../../uikit/Card'
 import Div from '../../uikit/Div'
 import ExampleCard from './ExampleCard'
 import ExampleGroup from './ExampleGroup'
+import GradientGrid from '../../uikit/BgLightEffect'
 
 export default function FlowBlocksExample() {
   useEffect(() => {
     attachPointerMoveCSSVariable()
   }, [])
 
-  const [boundingBox, setBoundingBox] = useState<DOMRect | undefined>()
-  const gridRef = useRef<HTMLDivElement>()
-
-  useEffect(() => {
-    if (gridRef.current) {
-      setBoundingBox(gridRef.current.getBoundingClientRect())
-    }
-  }, [])
   return (
     <ExampleCard title='FlowBlocksExample' category='misc' className='bg-block-dark'>
       <ExampleGroup caption='basic example'>
         {/* TODO: <BlockGen> 阵列器 */}
-        <Div
-          domRef={gridRef}
-          className='grid w-full gap-3'
-          style={[
-            genGridTemplate({ itemMinWidth: '10em' }),
-            {
-              background: `radial-gradient(circle at calc(var(--pointer-x) * 1px - ${
-                boundingBox?.left ?? 0
-              } * 1px) calc(var(--pointer-y) * 1px - ${boundingBox?.top ?? 0} * 1px), #fff5, transparent 160px)`
-            }
-          ]}
-        >
-          {Array.from({ length: 20 }, (_, idx) => (
-            <Card key={idx} className='bg-block-semi-dark w-full rounded' style={{ aspectRatio: '1 / 1' }} />
-          ))}
-        </Div>
+        <GradientGrid>
+          <Div className='grid w-full gap-3' style={[genGridTemplate({ itemMinWidth: '10em' })]}>
+            {Array.from({ length: 20 }, (_, idx) => (
+              <Card key={idx} className='bg-block-semi-dark w-full rounded' style={{ aspectRatio: '1 / 1' }} />
+            ))}
+          </Div>
+        </GradientGrid>
       </ExampleGroup>
     </ExampleCard>
   )
 }
-
 function attachPointerMoveCSSVariable() {
   document.addEventListener(
     'pointermove',
