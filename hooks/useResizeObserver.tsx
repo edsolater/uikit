d46898fdcabe3a06a41926a5e8ee642fdf0fc511
edsolater <edsolater@qq.com>
2021-10-03 12:01:02 +0800

    🎉(useResizeObserver) : add useResizeObserver

--- hooks/useResizeObserver.tsx
+++ hooks/useResizeObserver.tsx
@@ -1,24 +1,24 @@
 import { RefObject, useEffect } from 'react'
 
-/**
- * only itself(ref)
- *
- * this hooks build on assumption: resize of a child will resize his parent. so just observe it's parent node.
- *
- * @param ref
- * @param callback
- */
-export default function useResizeObserver(
-  ref: RefObject<HTMLElement | undefined | null> | undefined,
-  callback?: (entry: ResizeObserverEntry) => unknown
+import { Nullish } from '../typings/constants'
+
+/** for build-in ResizeObserverEntry need narrows down target manually which is tedious */
+interface GenericResizeObserverEntry<El extends Element> extends ResizeObserverEntry {
+  readonly target: El
+}
+
+export default function useResizeObserver<El extends Element>(
+  ref: RefObject<El | Nullish>,
+  callback: (entry: GenericResizeObserverEntry<El>, observer: ResizeObserver) => void
 ) {
   useEffect(() => {
-    if (!ref?.current) return
-    const observer = new window.ResizeObserver((entries) => {
-      for (const entry of entries) {
-        callback?.(entry)
-      }
+    if (!ref.current) return
+    const observer = new ResizeObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        callback(entry as any, observer)
+      })
     })
     observer.observe(ref.current)
+    return () => observer.disconnect()
   }, [])
 }
