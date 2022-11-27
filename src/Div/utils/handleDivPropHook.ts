b90d7d04d94dc5f5513ae2d3faf2fd32adbe7cb5
edsolater <edsolater@qq.com>
2022-11-28 01:42:51 +0800

    💡 all should be plugin

--- src/Div/utils/handleDivPropHook.ts
+++ src/Div/utils/handleDivPropHook.ts
@@ -2,6 +2,7 @@ import { flapDeep } from '@edsolater/fnkit'
 import { DivProps } from '../type'
 import { mergeProps } from './mergeProps'
 
+// TODO prop hook should just be a plugin
 export function handleDivPropHook<P extends Partial<DivProps<any>>>(props?: P): Omit<P, 'propHook'> | undefined {
   if (!props?.propHook) return props
   return flapDeep(props.propHook).reduce((acc, propHook) => mergeProps(acc, propHook(props)), props)
