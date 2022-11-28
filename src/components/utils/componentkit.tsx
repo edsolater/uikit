5440ca83fc0f8af428739c82885759abdf606941
edsolater <edsolater@qq.com>
2022-11-29 01:30:46 +0800

    🚧 componentkit may not always return a new component

--- src/components/utils/componentkit.tsx
+++ src/components/utils/componentkit.tsx
@@ -1,28 +1,21 @@
 import { isString, overwriteFunctionName } from '@edsolater/fnkit'
-import { useMemo } from 'react'
-import { Div, DivProps } from '../../Div'
+import { Div, DivChildNode, DivProps } from '../../Div'
 import { mergeProps } from '../../functions/react'
+import { AddProps } from '../AddProps'
 
-type Component<Props> = (props: Props) => JSX.Element
-
-type ComponentRootProps = DivProps
-type ComponentRoot = (componentkitProps?: ComponentRootProps) => JSX.Element
+type Component<Props> = (props: Props) => DivChildNode
+type ReactComponent<Props> = (props: Props) => JSX.Element
 
 export function componentkit<T>(
   options: { name: string } | string,
-  ComponentConstructerFn: (ComponentRoot: ComponentRoot) => Component<T>
-): Component<T & Omit<DivProps, 'children'>> {
+  ComponentConstructerFn: Component<T>
+): ReactComponent<T & Omit<DivProps, 'children'>> {
   const displayName = isString(options) ? options : options.name
-  const componentkitFC = overwriteFunctionName((props) => {
-    const KitRoot = useMemo(() => generateComponentRoot(props), [props])
-    return ComponentConstructerFn(KitRoot)(props) ?? null
-  }, displayName)
-  return componentkitFC
-}
-
-function generateComponentRoot(props: DivProps) {
-  const ComponentRoot = (componentkitProps?: ComponentRootProps) => (
-    <Div {...mergeProps(props, componentkitProps)}>{componentkitProps?.children}</Div> //every time merge props will get a different result
+  const componentkitFC = overwriteFunctionName(
+    (props) => (
+      <AddProps {...mergeProps(props, { className: displayName })}>{ComponentConstructerFn(props) ?? null}</AddProps>
+    ),
+    displayName
   )
-  return ComponentRoot
+  return componentkitFC
 }
--- src/components/utils/uikit.tsx
+++ src/components/utils/uikit.tsx
@@ -1,39 +1,21 @@
 import { isString, overwriteFunctionName } from '@edsolater/fnkit'
-import { useMemo } from 'react'
-import { Div, DivChildNode, DivProps } from '../../Div'
+import { DivChildNode, DivProps } from '../../Div'
 import { mergeProps } from '../../functions/react'
 import { AddProps } from '../AddProps'
 
 type Component<Props> = (props: Props) => DivChildNode
 type ReactComponent<Props> = (props: Props) => JSX.Element
 
-type UIKitRootProps = DivProps & {
-  /** @default 'Div' */
-  use?: 'Div' | 'AddProps'
-}
-
-type UIKitRoot = (uikitProps: UIKitRootProps) => DivChildNode
-
 export function uikit<T>(
   options: { name: string } | string,
   ComponentConstructerFn: Component<T>
 ): ReactComponent<T & Omit<DivProps, 'children'>> {
   const displayName = isString(options) ? options : options.name
-  const uikitFC = overwriteFunctionName((props) => {
-    return (
+  const uikitFC = overwriteFunctionName(
+    (props) => (
       <AddProps {...mergeProps(props, { className: displayName })}>{ComponentConstructerFn(props) ?? null}</AddProps>
-    )
-  }, displayName)
+    ),
+    displayName
+  )
   return uikitFC
-}
-
-function generateUIKitRoot(props: DivProps, displayName: string) {
-  console.log('props: ', props)
-  const KitRoot = ({ use, ...uikitProps }: UIKitRootProps) =>
-    use === 'AddProps' ? (
-      <AddProps {...mergeProps(props, uikitProps, { className: displayName })}>{uikitProps?.children}</AddProps>
-    ) : (
-      <Div {...mergeProps(props, uikitProps, { className: displayName })}>{uikitProps?.children}</Div>
-    )
-  return KitRoot
 }
