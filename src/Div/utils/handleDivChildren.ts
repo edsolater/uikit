e9384d0cf3ff81b5d2dab1619291951b6feb77a2
edsolater <edsolater@qq.com>
2022-12-04 01:06:45 +0800

    🚧 `createComponentContext()` can setVaule

--- src/Div/utils/handleDivChildren.ts
+++ src/Div/utils/handleDivChildren.ts
@@ -2,7 +2,7 @@ import { Cover, isArray, isObject } from '@edsolater/fnkit'
 import { isValidElement, ReactNode } from 'react'
 import { DivChildNode, DivProps } from '../type'
 
-export function parseDivChildren(children?: DivChildNode): ReactNode {
+export function parseDivChildrenToReactNode(children?: DivChildNode): ReactNode {
   if (isObject(children) && !isArray(children) && !isValidElement(children)) return String(children)
   return children as ReactNode
 }
@@ -11,5 +11,5 @@ export function handleDivChildren<P extends Partial<DivProps<any>>>(
   divProps?: P
 ): (Omit<P, 'childrend'> & { children?: ReactNode }) | undefined {
   if (!divProps) return
-  return { ...divProps, children: parseDivChildren(divProps.children) } as (Omit<P, 'childrend'> & { children?: ReactNode })
+  return { ...divProps, children: parseDivChildrenToReactNode(divProps.children) } as (Omit<P, 'childrend'> & { children?: ReactNode })
 }
--- src/components/utils/createComponentContext.ts
+++ src/components/utils/createComponentContext.ts
@@ -1,21 +1,46 @@
 import { assert } from '@edsolater/fnkit'
-import React, { createContext, FC, useContext } from 'react'
+import produce from 'immer'
+import React, { createContext, useContext, useState } from 'react'
 import { DivChildNode } from '../../Div'
-import { parseDivChildren } from '../../Div/utils/handleDivChildren'
+import { parseDivChildrenToReactNode } from '../../Div/utils/handleDivChildren'
 import { ReactComponent } from '../../typings/tools'
+
+type ComponentContextValue<Props> = {
+  contextComponentProps?: Props
+  contextComponentSet?: (setDraft: (state: Props) => void) => void
+}
 
 export function createComponentContext<Props extends Record<keyof any, any>>(
   defaultProps?: Props
-): [component: ReactComponent<Props & { children: DivChildNode }>, hook: () => Props & { children?: DivChildNode }] {
-  const context = createContext<(Props & { children?: DivChildNode }) | undefined>(defaultProps)
+): [
+  component: ReactComponent<Props & { children?: DivChildNode }>,
+  hook: () => ComponentContextValue<Props & { children?: DivChildNode }>
+] {
+  const context = createContext<ComponentContextValue<Props & { children?: DivChildNode }>>({
+    contextComponentProps: defaultProps
+  })
+
   const hook = () => {
-    const contextProp = useContext(context)
-    assert(contextProp, 'lack of Provider')
-    return contextProp
+    const { contextComponentProps, contextComponentSet } = useContext(context)
+    assert(contextComponentProps, 'lack of Provider')
+    assert(contextComponentSet, 'lack of Provider')
+    return { contextComponentProps: contextComponentProps, contextComponentSet: contextComponentSet }
   }
-  /** @Provider no need props!!  */
-  const Provider = (props: Props & { children: DivChildNode }) => {
-    return React.createElement(context.Provider, { value: props }, parseDivChildren(props.children))
+
+  const Provider = (props: Props & { children?: DivChildNode }) => {
+    const [store, innerSetStore] = useState(props)
+    const setStore = (innerFn) => {
+      innerSetStore((s) =>
+        produce(s, (draft) => {
+          innerFn(draft)
+        })
+      )
+    }
+    return React.createElement(
+      context.Provider,
+      { value: { contextComponentProps: store, contextComponentSet: setStore } },
+      parseDivChildrenToReactNode(props.children)
+    )
   }
   return [Provider, hook]
 }
