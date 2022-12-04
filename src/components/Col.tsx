1a0e00bc0e16b6ce99b2acd039cce3b2514a967e
edsolater <edsolater@qq.com>
2022-12-05 02:42:54 +0800

    🚧 richer `<Col>` and `<Row>`

--- src/components/Col.tsx
+++ src/components/Col.tsx
@@ -5,13 +5,22 @@ export interface ColProps {
   children?: DivChildNode
   wrap?: boolean
   name?: string // same as `<Group>`'s grid
+  /**@default 'md' */
+  gap?: 'sm' /* 4px */ | 'md' /* 8px */ | 'lg' /* 16px */ | 'xl' /* 32px */
 }
 
 /**
  * flex box
  */
-export const Col = uikit('Col', ({ wrap, children }: ColProps) => (
-  <Div icss={{ display: 'flex', flexDirection: 'column', flexWrap: wrap ? 'wrap' : undefined, alignItems: 'center' }}>
+export const Col = uikit('Col', ({ wrap, children, gap }: ColProps) => (
+  <Div
+    icss={{
+      display: 'flex',
+      flexDirection: 'column',
+      flexWrap: wrap ? 'wrap' : undefined,
+      gap: gap === 'xl' ? 32 : gap === 'lg' ? 16 : gap === 'sm' ? 4 : 8
+    }}
+  >
     {children}
   </Div>
 ))
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -4,15 +4,39 @@ import { uikit } from './utils'
 
 export interface RowProps {
   children?: DivChildNode
+  /** CSS */
   wrap?: boolean
+
+  /**@default 'md' */
+  gap?: 'sm' /* 4px */ | 'md' /* 8px */ | 'lg' /* 16px */ | 'xl' /* 32px */
+
+  /** CSS */
+  itemFlexGrow?: boolean
+
+  /** CSS */
+  spaceBetween?: boolean
+
   name?: string // same as `<Group>`'s grid
 }
 
 /**
  * flex box (default has alignItems:center justifyContent:space-between)
  */
-export const Row = uikit('Row', ({ children, wrap }: RowProps) => (
-  <Div icss={{ display: 'flex', flexWrap: wrap ? 'wrap' : undefined, alignItems: 'center' }}>{children}</Div>
+export const Row = uikit('Row', ({ children, itemFlexGrow, gap, wrap, spaceBetween }: RowProps) => (
+  <Div
+    icss={[
+      {
+        display: 'flex',
+        justifyContent: spaceBetween ? 'space-between' : undefined,
+        gap: gap === 'xl' ? 32 : gap === 'lg' ? 16 : gap === 'sm' ? 4 : 8,
+        flexWrap: wrap ? 'wrap' : undefined,
+        alignItems: 'center'
+      },
+      itemFlexGrow ? flexChildGrow : undefined
+    ]}
+  >
+    {children}
+  </Div>
 ))
 
 export const flexChildGrow: ICSS = {
