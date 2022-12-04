3263c0ff042080dba7dbcfe9d55607071c43669b
edsolater <edsolater@qq.com>
2022-12-04 16:35:44 +0800

    🚧 `createComponentContext()` can setVaule

--- src/components/utils/createComponentContext.ts
+++ src/components/utils/createComponentContext.ts
@@ -1,46 +1,64 @@
-import { assert } from '@edsolater/fnkit'
-import produce from 'immer'
+import { assert, overwriteFunctionName, pipe, shrinkToValue, uncapitalize } from '@edsolater/fnkit'
 import React, { createContext, useContext, useState } from 'react'
 import { DivChildNode } from '../../Div'
 import { parseDivChildrenToReactNode } from '../../Div/utils/handleDivChildren'
 import { ReactComponent } from '../../typings/tools'
 
-type ComponentContextValue<Props> = {
-  contextComponentProps?: Props
-  contextComponentSet?: (setDraft: (state: Props) => void) => void
+type ComponentContextValue<Props extends Record<keyof any, any>> = {
+  value: Props
+  set: React.Dispatch<React.SetStateAction<Props>>
 }
 
 export function createComponentContext<Props extends Record<keyof any, any>>(
-  defaultProps?: Props
+  defaultProps: Props
 ): [
   component: ReactComponent<Props & { children?: DivChildNode }>,
-  hook: () => ComponentContextValue<Props & { children?: DivChildNode }>
+  hook: () => ComponentContextValue<Props & { children?: DivChildNode }>,
+  propertyHooks: {
+    [key in keyof Props as `use${Capitalize<key & string>}`]: ComponentContextValue<
+      React.Dispatch<React.SetStateAction<Props[key]>>
+    >
+  }
 ] {
   const context = createContext<ComponentContextValue<Props & { children?: DivChildNode }>>({
-    contextComponentProps: defaultProps
+    value: defaultProps,
+    set: () => {
+      throw 'lack of Provider'
+    }
   })
 
   const hook = () => {
-    const { contextComponentProps, contextComponentSet } = useContext(context)
-    assert(contextComponentProps, 'lack of Provider')
-    assert(contextComponentSet, 'lack of Provider')
-    return { contextComponentProps: contextComponentProps, contextComponentSet: contextComponentSet }
+    const { value, set } = useContext(context)
+    assert(value, 'lack of Provider')
+    assert(set, 'lack of Provider')
+    return { value, set }
   }
+  const propertyHooks = new Proxy(
+    {},
+    {
+      get(target, p) {
+        const propertyName = pipe(String(p), (s) => s.match(/use(?<property>.*)/)?.groups?.property ?? '', uncapitalize)
+        return overwriteFunctionName(() => {
+          const { value: contextValue, set: contextSet } = useContext(context)
+          assert(contextValue, 'lack of Provider')
+          assert(contextSet, 'lack of Provider')
+          return {
+            value: contextValue[propertyName],
+            //@ts-ignore
+            set: (dispatcher) => contextSet((s) => ({ [propertyName]: shrinkToValue(dispatcher, [s]) }))
+          }
+        }, String(p))
+      }
+    }
+  )
 
   const Provider = (props: Props & { children?: DivChildNode }) => {
-    const [store, innerSetStore] = useState(props)
-    const setStore = (innerFn) => {
-      innerSetStore((s) =>
-        produce(s, (draft) => {
-          innerFn(draft)
-        })
-      )
-    }
+    const [store, setStore] = useState(props)
     return React.createElement(
       context.Provider,
-      { value: { contextComponentProps: store, contextComponentSet: setStore } },
+      { value: { value: store, set: setStore } },
       parseDivChildrenToReactNode(props.children)
     )
   }
-  return [Provider, hook]
+  return [Provider, hook, propertyHooks as any]
 }
