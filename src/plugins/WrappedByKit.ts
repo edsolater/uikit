7201097203cdd480948538bf7485a77360b9c491
edsolater <edsolater@qq.com>
2022-12-11 23:04:52 +0800

    🚚 WrappedBy

--- src/plugins/WrappedByKit.ts
+++ src/plugins/WrappedByKit.ts
@@ -3,6 +3,6 @@ import { Component } from '../typings/tools'
 import { createWrapperPlugin } from './createPlugin'
 
 /** render self node as first child of Wrapper */
-export const WrappedByKit = createWrapperPlugin(<T>(ComponentConstructor: Component<T>, props?: T) => {
+export const WrappedBy = createWrapperPlugin(<T>(ComponentConstructor: Component<T>, props?: T) => {
   return (node) => createElement(ComponentConstructor as any, props ?? {}, node)
 })
