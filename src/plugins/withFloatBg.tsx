2b031559f7d8b64c97ffde806361f82b69933939
edsolater <edsolater@qq.com>
2022-12-13 22:06:35 +0800

    🚧 add floatBg plugin;s motion props

--- src/plugins/withFloatBg.tsx
+++ src/plugins/withFloatBg.tsx
@@ -6,38 +6,40 @@ import { addEventListener, getSiblings } from '../utils'
 import { createNormalPlugin } from './createPlugin'
 import { WrappedBy } from './WrappedBy'
 
-export const withFloatBg = createNormalPlugin((oldProps) => (options?: { floatBgProps?: DivProps }) => {
-  const [dom, setDOM] = useDOM()
-  const [activeTab, setActiveTab] = useDOM()
+export const withFloatBg = createNormalPlugin(
+  (oldProps) => (options?: { floatBgProps?: DivProps; MotionProps?: MotionProps }) => {
+    const [dom, setDOM] = useDOM()
+    const [activeTab, setActiveTab] = useDOM()
 
-  useEffect(() => {
-    const subscriptions = getSiblings(dom).map((el) =>
-      addEventListener(el, 'click', ({ el }) => {
-        setActiveTab(el)
-      })
-    )
-    return () => subscriptions.forEach((subscription) => subscription.cancel())
-  }, [dom])
+    useEffect(() => {
+      const subscriptions = getSiblings(dom).map((el) =>
+        addEventListener(el, 'click', ({ el }) => {
+          setActiveTab(el)
+        })
+      )
+      return () => subscriptions.forEach((subscription) => subscription.cancel())
+    }, [dom])
 
-  return {
-    children: (
-      <>
-        <Div
-          shadowProps={options?.floatBgProps}
-          domRef={setDOM}
-          icss={{
-            width: activeTab?.offsetWidth,
-            height: activeTab?.offsetHeight,
-            top: activeTab?.offsetTop,
-            left: activeTab?.offsetLeft,
-            position: 'absolute',
-            background: '#d1d3d6'
-          }}
-          className='Tabs-active-bg-panel'
-          plugins={WrappedBy(Motion)}
-        ></Div>
-        {oldProps.children}
-      </>
-    )
+    return {
+      children: (
+        <>
+          <Div
+            shadowProps={options?.floatBgProps}
+            domRef={setDOM}
+            icss={{
+              width: activeTab?.offsetWidth,
+              height: activeTab?.offsetHeight,
+              top: activeTab?.offsetTop,
+              left: activeTab?.offsetLeft,
+              position: 'absolute',
+              background: '#d1d3d6'
+            }}
+            className='Tabs-active-bg-panel'
+            plugins={WrappedBy(Motion, options?.MotionProps)}
+          ></Div>
+          {oldProps.children}
+        </>
+      )
+    }
   }
-})
+)
