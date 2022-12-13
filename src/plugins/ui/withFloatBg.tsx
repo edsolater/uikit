3d6695d5cd78c33060b32a7432197f94078e76a3
edsolater <edsolater@qq.com>
2022-12-14 00:15:18 +0800

    🚧 `plugin:withfloatBg`  export `type:WithFloatBgOptions`

--- src/plugins/ui/withFloatBg.tsx
+++ src/plugins/ui/withFloatBg.tsx
@@ -6,42 +6,46 @@ import { addEventListener, getSiblings } from '../../utils'
 import { createPropPlugin } from '../createPlugin'
 import { WrappedBy } from '../misc/WrappedBy'
 
-export const withFloatBg = createPropPlugin(
-  (oldProps) => (options?: { floatBgProps?: DivProps; MotionProps?: MotionProps; defaultActiveItemIndex?: number }) => {
-    const [dom, setDOM] = useDOM()
-    const [activeTab, setActiveTab] = useDOM()
+export type WithFloatBgOptions = {
+  floatBgProps?: DivProps
+  MotionProps?: MotionProps
+  defaultActiveItemIndex?: number
+}
 
-    useLayoutEffect(() => {
-      const siblings = getSiblings(dom)
-      const subscriptions = siblings.map((el) =>
-        addEventListener(el, 'click', ({ el }) => {
-          setActiveTab(el)
-        })
-      )
-      if (options?.defaultActiveItemIndex) siblings.at(options.defaultActiveItemIndex)?.click()
-      return () => subscriptions.forEach((subscription) => subscription.cancel())
-    }, [dom])
+export const withFloatBg = createPropPlugin((oldProps) => (options?: WithFloatBgOptions) => {
+  const [dom, setDOM] = useDOM()
+  const [activeTab, setActiveTab] = useDOM()
 
-    return {
-      children: (
-        <>
-          <Div
-            shadowProps={options?.floatBgProps}
-            domRef={setDOM}
-            icss={{
-              width: activeTab?.offsetWidth,
-              height: activeTab?.offsetHeight,
-              top: activeTab?.offsetTop,
-              left: activeTab?.offsetLeft,
-              position: 'absolute',
-              background: '#d1d3d6'
-            }}
-            className='Tabs-active-bg-panel'
-            plugins={WrappedBy(Motion, options?.MotionProps)}
-          ></Div>
-          {oldProps.children}
-        </>
-      )
-    }
+  useLayoutEffect(() => {
+    const siblings = getSiblings(dom)
+    const subscriptions = siblings.map((el) =>
+      addEventListener(el, 'click', ({ el }) => {
+        setActiveTab(el)
+      })
+    )
+    if (options?.defaultActiveItemIndex) siblings.at(options.defaultActiveItemIndex)?.click()
+    return () => subscriptions.forEach((subscription) => subscription.cancel())
+  }, [dom])
+
+  return {
+    children: (
+      <>
+        <Div
+          shadowProps={options?.floatBgProps}
+          domRef={setDOM}
+          icss={{
+            width: activeTab?.offsetWidth,
+            height: activeTab?.offsetHeight,
+            top: activeTab?.offsetTop,
+            left: activeTab?.offsetLeft,
+            position: 'absolute',
+            background: '#d1d3d6'
+          }}
+          className='Tabs-active-bg-panel'
+          plugins={WrappedBy(Motion, options?.MotionProps)}
+        ></Div>
+        {oldProps.children}
+      </>
+    )
   }
-)
+})
