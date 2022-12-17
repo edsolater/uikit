05da3d6c2e20c60b24ce2dea756e999f7908ad4d
edsolater <edsolater@qq.com>
2022-12-17 23:54:26 +0800

    🐛 `<AccordionPanel>` won't clickable

--- src/components/Accordion/AccordionPanel.tsx
+++ src/components/Accordion/AccordionPanel.tsx
@@ -1,8 +1,7 @@
 import { assert, shrinkToValue } from '@edsolater/fnkit'
 import { ReactNode } from 'react'
-import { Div } from '../../Div/Div'
+import { Div } from '../../Div'
 import { DivProps } from '../../Div/type'
-import { icssClickable } from '../../styles'
 import { FadeIn } from '../FadeIn'
 import { componentKit } from '../utils'
 import { useAccordionContextProps } from './AccordionContext'
@@ -13,9 +12,7 @@ export const AccordionPanel = componentKit('AccordionPanel', ({ children }: Acco
   assert(controller, 'lack of accordion controller')
   return (
     <FadeIn show={contextProps.open} duration={200} transitionPresets={[]}>
-      <Div onClick={controller.toggle} icss={icssClickable}>
-        {shrinkToValue(children, [Boolean(contextProps.open), controller])}
-      </Div>
+      <Div>{shrinkToValue(children, [Boolean(contextProps.open), controller])}</Div>
     </FadeIn>
   )
 })
