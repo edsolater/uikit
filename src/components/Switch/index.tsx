b368a6f2f7ad4af3550a0b3e938573d588fd0f8e
edsolater <edsolater@qq.com>
2022-12-23 02:28:14 +0800

    🚧 richer `<Switch>`

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -7,34 +7,46 @@ import { letSwitchStyle, SwitchVariables } from './plugins/letSwitchStyle'
 import { FeatureDefaultCheck, letDefaultCheck } from './plugins/letDefaultCheck'
 import { ICSS } from '../../styles'
 
+export type SwitchStatus = {
+  checked: boolean
+}
+
 export interface SwitchCoreProps {
+  // -------- core --------
   checked?: boolean
   onToggle?: (toStatus: boolean) => void
 
-  renderThumbIcon?: MayFn<ReactNode>
-  anatomy?: {
-    track?: DivProps
-    thumb?: DivProps
+  // -------- sub --------
+  render?: {
+    thumbIcon?: MayFn<ReactNode, [utils: SwitchStatus]>
   }
-  // enrich DivProps's icss
-  icss?: ICSS<SwitchVariables> // TODO: <-- this is weird
+  anatomy?: {
+    track?: MayFn<DivProps, [utils: SwitchStatus]>
+    thumb?: MayFn<DivProps, [utils: SwitchStatus]>
+  }
+
+  // -------- enrich DivProps's icss --------
+  icss?: ICSS<SwitchVariables>
 }
 
 export type SwitchProps = SwitchCoreProps & FeatureDefaultCheck
 
 export const Switch = uikit(
   'Switch',
-  ({ checked, onToggle, renderThumbIcon, anatomy }: SwitchProps) => {
+  ({ checked, onToggle, render, anatomy }: SwitchProps) => {
+    const status = {
+      checked: Boolean(checked)
+    }
     return (
       <Div
         className='Switch-track'
-        shadowProps={anatomy?.track}
+        shadowProps={shrinkToValue(anatomy?.track, [status])}
         plugin={click(() => {
-          onToggle?.(!checked)
+          onToggle?.(!status.checked)
         })}
       >
-        <Div className='Switch-thumb' shadowProps={anatomy?.thumb}>
-          {shrinkToValue(renderThumbIcon)}
+        <Div className='Switch-thumb' shadowProps={shrinkToValue(anatomy?.thumb, [status])}>
+          {shrinkToValue(render?.thumbIcon, [status])}
         </Div>
       </Div>
     )
