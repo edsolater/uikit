322b74ce3de0fcec2642db49c412556acc9e4ce8
zhanggongying <edsolater@qq.com>
2023-01-11 17:40:39 +0800

    🚧 enrich `<For>`

--- src/components/For.tsx
+++ src/components/For.tsx
@@ -1,3 +1,4 @@
+import { isNumber, isString } from '@edsolater/fnkit'
 import { DivProps } from '../Div'
 import { addPropsToReactElement } from '../utils/react/addPropsToReactElement'
 import { createKit } from './utils'
@@ -8,10 +9,11 @@ export interface ForProps<T extends any> extends Omit<DivProps, 'children'> {
   children?: (item: T, idx: number) => JSX.Element
 }
 
-export const For = createKit('For', <T extends any>(props: ForProps<T>) =>
-  props.each.map((item, idx) =>
-    props.getKey
-      ? addPropsToReactElement(props.children?.(item, idx), { key: props.getKey(item, idx) })
-      : props.children?.(item, idx)
+export const For = createKit('For', <T extends any>(props: ForProps<T>) => {
+  function spawnKey(item: T, idx: number) {
+    return props.getKey ? props.getKey(item, idx) : isString(item) || isNumber(item) ? item : idx
+  }
+  return props.each.map((item, idx) =>
+    addPropsToReactElement(props.children?.(item, idx), { key: spawnKey(item, idx) })
   )
-)
+})
