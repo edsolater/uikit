ad32f97629eb1f76e38cc5961fdef29fd2800eea
edsolater <edsolater@gmail.com>
2026-05-14 13:32:34 +0800

    🚧 原生 .d.ts 的引用要使用 refrence

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
  * 包发布入口。
  * 对外 API 先由所属目录出口收口，再从这里统一暴露。
  */
-import './types/htmlElementViewTransition'
-import './types/htmlPopover'
+/// <reference path="./types/htmlElementViewTransition.d.ts" />
+/// <reference path="./types/htmlPopover.d.ts" />
 
 export * from './components'
 export * from './hooks'
